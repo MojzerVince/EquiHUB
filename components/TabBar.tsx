48eba6ab3be0791664041797e41d03d13f6b9d79
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2025-07-28 20:29:09 +0200

    added bar icons as component

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -3,49 +3,11 @@ import { useLinkBuilder, useTheme } from "@react-navigation/native";
 import { PlatformPressable } from "@react-navigation/elements";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { StyleSheet } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { icons } from "@/components/icon";
 
 export function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
   const { colors } = useTheme();
   const { buildHref } = useLinkBuilder();
-  const icons = {
-    club: (props: any) => (
-      <Image
-        source={require("../assets/UI_resources/UI_white/club_white.png")}
-        style={{ width: 48, height: 48 }}
-      ></Image>
-    ),
-    coach: () => (
-      <Image
-        source={require("../assets/UI_resources/UI_white/coach_white.png")}
-        style={{ width: 48, height: 48 }}
-      ></Image>
-    ),
-    map: () => (
-      <Image
-        source={require("../assets/UI_resources/UI_white/map_white.png")}
-        style={{ width: 48, height: 48 }}
-      ></Image>
-    ),
-    index: () => (
-      <Image
-        source={require("../assets/UI_resources/UI_white/horse_white.png")}
-        style={{ width: 48, height: 48 }}
-      ></Image>
-    ),
-    market: () => (
-      <Image
-        source={require("../assets/UI_resources/UI_white/shop_white.png")}
-        style={{ width: 48, height: 48 }}
-      ></Image>
-    ),
-    profile: () => (
-      <Image
-        source={require("../assets/UI_resources/UI_white/user_white.png")}
-        style={{ width: 48, height: 48 }}
-      ></Image>
-    ),
-  };
 
   return (
     <View style={styles.tabBar}>
