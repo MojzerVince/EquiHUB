298b8372e73a3ba1b2e49b717efddc188971a2fe
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2025-07-28 20:55:32 +0200

    started working on my profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -10,7 +10,36 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileScreen = () => {
-  return <View></View>;
+  return (
+    <View>
+      <SafeAreaView style={styles.top_bar}>
+        <Text style={styles.header}>My Profile</Text>
+      </SafeAreaView>
+      <ScrollView style={styles.viewPort}></ScrollView>
+    </View>
+  );
 };
+
+const data = [];
+
+const styles = StyleSheet.create({
+  top_bar: {
+    backgroundColor: "#335C67",
+    height: "16%",
+  },
+  header: {
+    fontSize: 30,
+    fontFamily: "Inder",
+    color: "#fff",
+    textAlign: "center",
+    marginTop: -4,
+  },
+  viewPort: {
+    backgroundColor: "#FFFFFF",
+    height: "100%",
+    marginTop: -80,
+    borderRadius: 50,
+  },
+});
 
 export default ProfileScreen;
