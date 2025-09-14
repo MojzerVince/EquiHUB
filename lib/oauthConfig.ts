d696ef41ab5951a4cc662cc65e313408180ec591
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2025-09-14 21:38:05 +0200

    fix: update webClientId in oauthConfig for Supabase integration

--- lib/oauthConfig.ts
+++ lib/oauthConfig.ts
@@ -4,7 +4,7 @@ export const oauthConfig = {
     // You'll need to replace these with your actual OAuth 2.0 client IDs from Google Cloud Console
     //iosClientId: 'YOUR_IOS_CLIENT_ID.apps.googleusercontent.com',
     androidClientId: '645905000706-r5d9rejr3lakueqrhl1tk7ldmpv2jt2v.apps.googleusercontent.com',
-    //webClientId: 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com', // This is the web client ID that should be used for Supabase
+    webClientId: '645905000706-84poben7qoa735imq8ukp765ho5k0d97.apps.googleusercontent.com', // This is the web client ID that should be used for Supabase
     scopes: ['openid', 'profile', 'email'],
   },
   apple: {
